3aae5660e5ee49b5cf51aef87e1e7b8b6affb6f6
C'tri <CtriCoding@Ctri.co.uk>
2023-06-15 23:19:01 +0100

    further development on UI

--- ui.py
+++ ui.py
@@ -1,36 +1,66 @@
+from rich.console import RenderableType
 import textual
 
 from textual.app import App, ComposeResult
-from textual.containers import ScrollableContainer
+from textual.containers import Container, Vertical, Horizontal, Grid
 from textual.widgets import Button, Header, Footer, Static
 
 
-class GameDisplay(Static):
-    pass
-
-
-class TextDisplay(Static):
-    pass
-
-
-class Game(App):
+class Presence(App):
     """A simple stopwatch app."""
 
-    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+    BINDINGS = [
+        ("d", "toggle_dark", "Toggle dark mode"),
+        ("g", "hide_grid", "Toggle grid"),
+    ]
     CSS_PATH = "ui.css"
+    status = False
+
+    def action_toggle_grid(self) -> None:
+        self.status = not self.status
+        if not self.status:
+            self.query("control_grid").last().remove()
+        else:
+            self.query_one("#control_grid_holder").mount(self.create_control_grid())
 
     def compose(self) -> ComposeResult:
+        control_grid = self.create_control_grid()
         yield Header()
         yield Static("Image", classes="box")
-        yield Static("Console", classes="box")
+        if self.status:
+            yield Container(
+                Vertical(
+                    Static("Presence controls"),
+                    id="control_grid_holder",
+                ),
+                classes="console box",
+                id="right_console",
+            )
+        else:
+            yield Static("Console blob would go here")
         yield Static("input space", classes="input_box")
 
         yield Footer()
         return super().compose()
 
+    def create_control_grid(self) -> Grid:
+        # does this need to be Return or Yield????? this is breaking because everything is a generator and this isn't, I think.
+        yield (
+            Grid(
+                Button("fetch current", classes="control_button third"),
+                Button("Skip", classes="control_button third"),
+                Button("Solve", classes="control_button third"),
+                Button("refresh tasks", classes="control_button wide"),
+                Button("dividers", classes="control_button wide"),
+                classes="control_grid",
+                id="control_grid",
+            ),
+        )
+
 
 if __name__ == "__main__":
-    app = Game()
+    app = Presence()
+    app.action_toggle_grid()
     app.run()
 
 
