8ade85eacc70e3852f1b1cf334ab3f2145eed521
C'tri Goudie <CtriCoding@Ctri.co.uk>
2023-06-22 16:25:36 +0100

    progrses.py now has actions log

--- progress.py
+++ progress.py
@@ -2,10 +2,10 @@ import textual
 from textual.app import App, ComposeResult, RenderResult
 from textual.reactive import reactive
 from textual.widgets import Header, Footer
-from textual.containers import Container, Vertical
+from textual.containers import Container, Center
 from datetime import datetime
 from textual.screen import Screen
-from textual.widgets import Static
+from textual.widgets import Static, ProgressBar, Label
 from serviceHelpers.trello import trello
 import os
 
@@ -24,48 +24,156 @@ class DisplayWidget(Static):
 
 
 class Progress(App):
+    CSS_PATH = "progress.css"
     BINDINGS = [
         ("g", "update_trello_actions", "Get progress"),
-        ("o", "update_output_text", "Update output text"),
     ]
     output = reactive("actions go here")
+
     trello_actions = []
     T_HELPER = trello(BOARD_ID, TRELLO_KEY, TRELLO_TOKEN)
     last_action_received_id = reactive(datetime.now().strftime("%Y-%m-%d"))
+    new_cards = 0
+    solved_cards = 0
+    total_cards = 0
+    seen_card_ids = []
+    TARGET_LIST_ID = "5f0dee6c5026590ce3004732"
 
     def compose(self) -> ComposeResult:
         yield Header(show_clock=True)
-        yield DisplayWidget(self.output)
+        yield Container(
+            DisplayWidget(self.output),
+            Center(
+                Label("Task progress: "),
+                ProgressBar(
+                    total=self.total_cards,
+                    show_bar=True,
+                    show_percentage=True,
+                    show_eta=False,
+                    name="Tasks percentage",
+                ),
+            ),
+        )
         yield Footer()
 
     def on_mount(self) -> None:
+        """Called when the app is mounted."""
+        self.seen_card_ids = [
+            c["id"]
+            for c in self.T_HELPER.fetch_trello_cards()
+            if c["idList"] == self.TARGET_LIST_ID
+        ]
+        self.total_cards = len(self.seen_card_ids)
         self.action_update_trello_actions()
-        self.action_update_output_text()
+        self.set_interval(5, self.action_update_trello_actions)
+        self.set_interval(5, self.update_progress_bar)
 
-    def action_update_output_text(self):
+    def update_output_text(self):
+        """Update the output text"""
         self.query_one(DisplayWidget).output = self.output
 
+    def update_progress_bar(self):
+        """Update the progress bar"""
+        progress_bar = self.query_one(ProgressBar)
+        progress_bar.update(progress=self.solved_cards, total=self.total_cards)
+
     def action_update_trello_actions(self):
-        print(self.output)
         """Update the list of actions with the latest actions"""
         actions = self.T_HELPER.fetch_actions_for_board(
             since=self.last_action_received_id, limit=1000
         )
-        if len(actions) > 0:
-            self.last_action_received_id = actions[0]["id"]
-            self.trello_actions = actions + self.trello_actions
+        if len(actions) == 0:
+            return
+        self.last_action_received_id = actions[0]["id"]
+        self.trello_actions = actions + self.trello_actions
         output = ""
+        counter = -1
         for action in self.trello_actions:
+            counter += 1
+            action = self.recategorize_actions(action)
+            action = self.colour_action(action)
+            self.update_totals_from_action(action)
+            if action is None:
+                continue
+            output += f"[{action['colour']}]"
+            output += f"{action['type'] }"
+            output += f" {action['data']['card']['id'][-5:]} "
+            if len(action["subtypes"]) > 0:
+                output += "- "
+                output += ", ".join(action["subtypes"])
+            output += f" [/{action['colour']}]"
             if (
                 "data" in action
                 and "card" in action["data"]
                 and "name" in action["data"]["card"]
             ):
-                output += f"{action['type']}: {action['data']['card']['name']} {action['date']}\n"
-            else:
-                output += f"{action['type']}: {action['date']}\n"
+                output += f"{action['data']['card']['name']} "
+
+                output += f"\t{action['date']}"
+            output += "\n"
         self.output = output
-        return
+        self.update_output_text()
+
+    def update_totals_from_action(self, action: dict, initial=False):
+        """Update the total, new, and solved cards counts based on the action."""
+        # if a card is deleted or archived, then we increase the solve count
+
+        if action["type"] == "deleteCard":
+            self.solved_cards += 1
+        elif action["type"] == "updateCard" and "archive" in action["subtypes"]:
+            self.solved_cards += 1
+
+        # if a card is new, fetch it and increase the new & total cards count.
+        if action["type"] == "createCard":
+            self.new_cards += 1
+
+        return None
+
+    def recategorize_actions(self, action) -> dict:
+        """adds 'archiveCard' and 'moveCard' to the kinds of actions that can be returned."""
+        action["subtypes"] = []
+        if action["type"] == "updateCard":
+            old_data = action["data"].get("old", {})
+            if "due" in old_data:
+                action["subtypes"].append("due")
+            if "idList" in old_data:
+                action["subtypes"].append("moved_list")
+            if "name" in old_data or "desc" in old_data:
+                action["subtypes"].append("rewrite")
+            # if 'due', 'idlist', and 'name' not in old_data:
+            if "pos" in old_data:
+                action["subtypes"].append("reorder")
+            if "closed" in old_data and not old_data["closed"]:
+                action["subtypes"].append("archive")
+
+            if not any(
+                x in old_data
+                for x in ["due", "idList", "name", "pos", "closed", "desc"]
+            ):
+                action["subtypes"].append("other")
+        return action
+
+    def colour_action(self, action) -> dict:
+        action["colour"] = "white"
+
+        # don't have a convenient way of ordering, so used ifs instead of elifs as a way of prioritizing
+
+        # new tasks are red
+        if action["type"] == "createCard":
+            action["colour"] = "red"
+
+        if "due" in action["subtypes"]:
+            action["colour"] = "red"
+
+        # paused tasks are yellow
+        if "moved_list" in action["subtypes"]:
+            action["colour"] = "yellow"
+
+        # completed tasks are green
+        if "archive" in action["subtypes"] or action["type"] == "deleteCard":
+            action["colour"] = "green"
+
+        return action
 
 
 if __name__ == "__main__":
--- ui.py
+++ ui.py
@@ -9,6 +9,8 @@ from textual.containers import Container, Vertical, Horizontal, Grid
 from textual.dom import DOMNode
 from textual.widgets import Button, Header, Footer, Static, Placeholder
 from textual.reactive import reactive
+from textual.widgets import Static, ProgressBar
+from progress import DisplayWidget
 
 
 class Presence(App):
@@ -16,69 +18,21 @@ class Presence(App):
 
     BINDINGS = [
         ("d", "toggle_dark", "Toggle dark mode"),
-        ("g", "toggle_grid", "Toggle grid"),
+        ("g", "progress", "Progress the bar"),
     ]
     CSS_PATH = "ui.css"
     status = False
 
-    def action_toggle_grid(self) -> None:
-        self.status = not self.status
-        if not self.status:
-            self.query("control_grid").last().remove()
-        else:
-            result = self.query("Vertical")
-            result.last().mount(self.create_control_grid())
+    def action_progress(self) -> None:
+        self.query_one(ProgressBar).advance(1)
 
     def compose(self) -> ComposeResult:
         yield Header(show_clock=True)
-        yield LogDisplay()
-        if self.status:
-            yield Container(
-                Vertical(
-                    Static("Presence controls"),
-                    id="control_grid_holder",
-                ),
-                classes="console box",
-                id="right_console",
-            )
-        else:
-            yield Static("Console blob would go here")
-        yield Static("input space", classes="input_box")
-
+        yield ProgressBar(45g)
+        yield DisplayWidget()
+        yield ProgressBar(45)
         yield Footer()
         return super().compose()
-
-    def create_control_grid(self) -> Grid:
-        # does this need to be Return or Yield????? this is breaking because everything is a generator and this isn't, I think.
-        return Grid(
-            classes="control_grid",
-            id="control_grid",
-        )
-
-    def populate_control_grid(self, grid: Grid) -> Grid:
-        grid.mount(
-            Button("fetch current", classes="control_button third"),
-            Button("Skip", classes="control_button third"),
-            Button("Solve", classes="control_button third"),
-            Button("refresh tasks", classes="control_button wide"),
-            Button("dividers", classes="control_button wide"),
-        )
-
-
-class LogDisplay(Static):
-    start_time = reactive(monotonic)
-    time = reactive(0.0)
-
-    def on_mount(self) -> None:
-        self.set_interval(1, self.update_time)
-
-    def update_time(self) -> None:
-        self.time = monotonic() - self.start_time
-
-    def watch_time(self, time: float) -> None:
-        minutes, seconds = divmod(time, 60)
-        hours, minutes = divmod(minutes, 60)
-        self.update(f"{hours:02,.0f}:{minutes:02.0f}:{seconds:06.3f}")
 
 
 if __name__ == "__main__":
